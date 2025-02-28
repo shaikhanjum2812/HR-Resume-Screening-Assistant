29e9aa2343c4f9adbd5f54185e65a297cf21e8d0
shaikhanjum2812 <shaikhanjum2812@gmail.com>
2025-02-28 16:50:57 +0530

    Update database.py

--- src/database.py
+++ src/database.py
@@ -208,7 +208,7 @@ class Database:
                 result, justification, match_score, confidence_score,
                 years_experience_total, years_experience_relevant, years_experience_required,
                 meets_experience_requirement, key_matches, missing_requirements,
-                experience_analysis, overall_fit_score, interview_focus, skill_gaps,
+                experience_analysis, interview_focus, skill_gaps,
                 technical_depth, problem_solving_score, project_complexity_score,
                 implementation_experience_score, project_expertise_score,
                 experience_quality_score,
@@ -216,7 +216,7 @@ class Database:
                 resume_file_data, resume_file_type
             ) VALUES (
                 %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 
-                %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
+                %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
             )
         '''
 
